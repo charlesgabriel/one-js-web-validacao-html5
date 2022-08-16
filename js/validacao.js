4126613bd848093d4b9edfb4a9baf128ce324c3b
charlesgabriel <charlesgabriel@hotmail.com.br>
2022-08-15 21:22:32 -0300

    refatorado código para validar vários tipos de input/refactored code to validade various types of input

--- js/validacao.js
+++ js/validacao.js
@@ -1,8 +1,13 @@
-const dataNascimento = document.querySelector('#nascimento');
+export function valida(input) {
+    const tipoDeInput = input.dataset.tipo;
+    if(validadores[tipoDeInput]) {
+        validadores[tipoDeInput](input);
+    }
+}
 
-dataNascimento.addEventListener('blur', (evento) => {
-    validaDataNascimento(evento.target);
-});
+const validadores = {
+    dataNascimento:input => validaDataNascimento(input),
+}
 
 function validaDataNascimento(input) {
     const dataRecebida = new Date(input.value);
