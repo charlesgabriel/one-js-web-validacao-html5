72fad5155d7a301e6fab6f99ae5e49d2d0f0892d
charlesgabriel <charlesgabriel@hotmail.com.br>
2022-08-16 20:54:13 -0300

    criado mensagem de erro customizadas/created custom error message

--- js/validacao.js
+++ js/validacao.js
@@ -3,10 +3,53 @@ export function valida(input) {
     if(validadores[tipoDeInput]) {
         validadores[tipoDeInput](input);
     }
+    if(input.validity.valid) {
+        input.parentElement.classList.remove('input-container--invalido');
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = '';
+    } else {
+        input.parentElement.classList.add('input-container--invalido');
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = mostraMensagemDeErro(tipoDeInput, input);
+    }
 }
+
+const tiposDeErro = [
+    'valueMissing',
+    'typeMismatch',
+    'patternMismatch',
+    'customError',
+];
+
+const mensagensDeErro = {
+    nome: {
+        valueMissing: 'O campo nome não pode estar vazio.',
+    },
+    email: {
+        valueMissing: 'O campo email não pode estar vazio.',
+        typeMismatch: 'O email digitado não é válido.',
+    },
+    senha: {
+        valueMissing: 'O campo senha não pode estar vazio.',
+        patternMismatch: 'A senha deve conter entre 6 e 12 caracteres, de conter pelo menos uma letra maiúscula, um número e não deve conter símbolos.',
+    },
+    dataNascimento: {
+        valueMissing: 'O campo data de nascimento não pode estar vazio.',
+        customError: 'Você deve ser maior de 18 anos para se cadastrar.',
+    },
+};
 
 const validadores = {
     dataNascimento:input => validaDataNascimento(input),
+}
+
+function mostraMensagemDeErro(tipoDeInput, input) {
+    let mensagem = '';
+    tiposDeErro.forEach((erro) => {
+        if(input.validity[erro]) {
+            mensagem = mensagensDeErro[tipoDeInput][erro];
+            console.log(mensagem);
+        }
+    });
+    return mensagem;
 }
 
 function validaDataNascimento(input) {
