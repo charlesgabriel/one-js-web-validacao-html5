0cf87a65c64fcdb1dec50efe3e1ca009fa45af3d
charlesgabriel <charlesgabriel@hotmail.com.br>
2022-08-16 22:16:22 -0300

    criado validador de cpf com números repetidos/created cpf validator with repeated numbers

--- js/validacao.js
+++ js/validacao.js
@@ -21,24 +21,29 @@ const tiposDeErro = [
 
 const mensagensDeErro = {
     nome: {
-        valueMissing: 'O campo nome não pode estar vazio.',
+        valueMissing: 'O campo Nome não pode estar vazio.',
     },
     email: {
-        valueMissing: 'O campo email não pode estar vazio.',
+        valueMissing: 'O campo Email não pode estar vazio.',
         typeMismatch: 'O email digitado não é válido.',
     },
     senha: {
-        valueMissing: 'O campo senha não pode estar vazio.',
+        valueMissing: 'O campo Senha não pode estar vazio.',
         patternMismatch: 'A senha deve conter entre 6 e 12 caracteres, de conter pelo menos uma letra maiúscula, um número e não deve conter símbolos.',
     },
     dataNascimento: {
-        valueMissing: 'O campo data de nascimento não pode estar vazio.',
+        valueMissing: 'O campo Data de Nascimento não pode estar vazio.',
         customError: 'Você deve ser maior de 18 anos para se cadastrar.',
+    },
+    cpf: {
+        valueMissing: 'O campo CPF não pode estar vazio.',
+        customError: 'O CPF digitado não é válido.',
     },
 };
 
 const validadores = {
     dataNascimento:input => validaDataNascimento(input),
+    cpf:input => validaCPF(input),
 }
 
 function mostraMensagemDeErro(tipoDeInput, input) {
@@ -69,3 +74,38 @@ function maiorQue18(data) {
 
     return dataMais18 <= dataAtual;
 }
+
+function validaCPF(input) {
+    const cpfFormatado = input.value.replace(/\D;/g, '');
+    let mensagem = '';
+
+    if(!checaCPFRepedito(cpfFormatado)) {
+        mensagem = 'O CPF digitado não é válido.';
+    }
+
+    input.setCustomValidity(mensagem);
+}
+
+function checaCPFRepedito(cpf) {
+    const valoresRepetidos = [
+        '00000000000',
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999',
+    ];
+    let cpfValido = true;
+
+    valoresRepetidos.forEach((valor) => {
+        if(valor == cpf) {
+            cpfValido = false;
+        }
+    });
+
+    return cpfValido;
+} 
