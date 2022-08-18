80cf865ccaa86f9c901214cdc1c510825f1c9ae1
charlesgabriel <charlesgabriel@hotmail.com.br>
2022-08-18 20:32:40 -0300

    criado validação de cpf com números válidos/created cpf validation with valid numbers

--- js/validacao.js
+++ js/validacao.js
@@ -79,7 +79,7 @@ function validaCPF(input) {
     const cpfFormatado = input.value.replace(/\D;/g, '');
     let mensagem = '';
 
-    if(!checaCPFRepedito(cpfFormatado)) {
+    if(!checaCPFRepedito(cpfFormatado) || !checaEstruturaCPF(cpfFormatado)) {
         mensagem = 'O CPF digitado não é válido.';
     }
 
@@ -108,4 +108,31 @@ function checaCPFRepedito(cpf) {
     });
 
     return cpfValido;
-} 
+}
+
+function checaEstruturaCPF(cpf) {
+    const multiplicador = 10;
+    return checaDigitoVerificardor(cpf, multiplicador);
+}
+
+function checaDigitoVerificardor(cpf, multiplicador) {
+    if(multiplicador >= 12) {
+        return true;
+    }
+    let multiplicadorInicial = multiplicador;
+    let soma = 0;
+    const cpfSemDigitos = cpf.substr(0, multiplicador - 1).split('');
+    const digitoVerificador = cpf.charAt(multiplicador - 1);
+    for(let contador = 0; multiplicadorInicial > 1; multiplicadorInicial--) {
+        soma = soma + cpfSemDigitos[contador] * multiplicadorInicial
+        contador++
+    }
+    if(digitoVerificador == confirmaDigito(soma)) {
+        return checaDigitoVerificardor(cpf, multiplicador + 1);
+    }
+    return false;
+}
+
+function confirmaDigito(soma) {
+    return 11 - (soma % 11);
+}
