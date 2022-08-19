0b8be6b75e5dd6246ad86b137827bff09790c5f2
charlesgabriel <charlesgabriel@hotmail.com.br>
2022-08-18 21:17:09 -0300

    preenchido os campos de endereço com dados da api viacep/filled in the address fields with data from the viacep api

--- js/validacao.js
+++ js/validacao.js
@@ -42,6 +42,7 @@ const mensagensDeErro = {
     cep: {
         valueMissing: 'O campo CEP não pode estar vazio.',
         patternMismatch: 'O CEP digitado não é válido.',
+        customError: 'Não foi possível buscar o CEP.',
     },
     logradouro: {
         valueMissing: 'O campo Logradouro não pode estar vazio.',
@@ -164,8 +165,24 @@ function recuperarCEP(input) {
     if(!input.validity.patternMismatch && !input.validity.valueMissing) {
         fetch(url, options).then(
             response => response.json()
-        ).then(
-            data => console.log(data)
-        );
+        ).then(data => {
+            if(data.erro) {
+                input.setCustomValidity('Não foi possível buscar o CEP.');
+                return;
+            }
+            input.setCustomValidity('');
+            preencheCamposComCEP(data);
+            return;
+        });
     }
+}
+
+function preencheCamposComCEP(data) {
+    const logradouro = document.querySelector('[data-tipo="logradouro"]');
+    const cidade = document.querySelector('[data-tipo="cidade"]');
+    const estado = document.querySelector('[data-tipo="estado"]');
+
+    logradouro.value = data.logradouro;
+    cidade.value = data.localidade;
+    estado.value = data.uf;
 }
